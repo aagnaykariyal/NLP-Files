8a4e539ddd5c73b96f9bc2c9733e0667e803d2fd
aagnaykariyal <aagnaykariyal@gmail.com>
2024-01-10 15:21:10 -0500

    UI Changes with customtkinter

--- Sentiment Analysis Using Bert/main.py
+++ Sentiment Analysis Using Bert/main.py
@@ -1,57 +1,59 @@
 import pickle
-import tkinter
-from reddit import RedditClass  # Importing the reddit.py file
-from sentiment import Sentiment  # Importing the required functions from sentiment.py file
+import customtkinter
+from reddit import RedditClass
+from sentiment import Sentiment
 
 sentiment = Sentiment()
 
-root = tkinter.Tk()
-root.title("Sentiment Analysis Using RoBERTa")
-root.geometry("400x200")
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme('green')
 
+root = customtkinter.CTk()
+root.title('Sentiment Analysis with RoBERTa')
+root.geometry("500x350")
 
-def button_press():
-    tkinter.Label(root, text=var1.get())
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+def button_press ():
     if var1.get() == 1:
         reddit = RedditClass()
         reddit.redditapi()
-        tkinter.Label(root, text="Reddit data has been run").grid()
+        customtkinter.CTkLabel(frame, text="Reddit data has been run").pack()
     if var2.get() == 1:
         sentiment.run_sentiments()
-        tkinter.Label(root, text="Sentiment has been assigned to the data").grid()
+        customtkinter.CTkLabel(frame, text="Sentiment has been assigned to the data").pack()
     if var3.get() == 1:
         model, vectorizer, train_acc, test_acc = sentiment.run_model()
 
         def save_model():
             pickle.dump(model, open('SentimentModel.pkl', 'wb'))
             pickle.dump(vectorizer, open('Vectorizer.pkl', 'wb'))
-            tkinter.Label(root, text="Model has been saved").grid()
+            customtkinter.CTkLabel(root, text="Model has been saved").pack()
             custom_dialog.destroy()
 
-        tkinter.Label(root, text="Model has been trained").grid()
-        custom_dialog = tkinter.Toplevel(root)
+        customtkinter.CTkLabel(frame, text="Model has been trained").pack()
+        custom_dialog = customtkinter.CTkToplevel(root)
         custom_dialog.title('Save Model')
-        tkinter.Label(custom_dialog, text=f"Training data accuracy: {train_acc}").grid()
-        tkinter.Label(custom_dialog, text=f"Test data accuracy: {test_acc}").grid()
-        tkinter.Label(custom_dialog, text="Do you want to save the model?").grid()
-        tkinter.Button(custom_dialog, text="Yes", command=save_model).grid()
-        tkinter.Button(custom_dialog, text="No", command=custom_dialog.destroy).grid()
+        customtkinter.CTkLabel(custom_dialog, text=f"Training data accuracy: {train_acc}").pack()
+        customtkinter.CTkLabel(custom_dialog, text=f"Test data accuracy: {test_acc}").pack()
+        customtkinter.CTkLabel(custom_dialog, text="Do you want to save the model?").pack()
+        customtkinter.CTkButton(custom_dialog, text="Yes", command=save_model).pack()
+        customtkinter.CTkButton(custom_dialog, text="No", command=custom_dialog.destroy).pack()
 
+var1 = customtkinter.IntVar()
+var2 = customtkinter.IntVar()
+var3 = customtkinter.IntVar()
 
-var1 = tkinter.IntVar()
-var2 = tkinter.IntVar()
-var3 = tkinter.IntVar()
+customtkinter.CTkCheckBox(frame, text="Get Reddit Data", variable=var1).pack(pady=5, padx=10)
+customtkinter.CTkCheckBox(frame, text="Get Sentiment Data", variable=var2).pack(pady=5, padx=10)
+customtkinter.CTkCheckBox(frame, text="Train the model", variable=var3).pack(pady=5, padx=10)
 
-tkinter.Checkbutton(root, text="Get Reddit Data", variable=var1).grid(row=0, column=1)
-tkinter.Checkbutton(root, text="Get Sentiment Data", variable=var2).grid(row=1, column=1)
-tkinter.Checkbutton(root, text="Train the model", variable=var3).grid(row=2, column=1)
+customtkinter.CTkButton(frame, text="Run", command=button_press).pack(pady=6, padx=10)
 
-tkinter.Button(root, text="Run", command=button_press).grid(row=4, column=1)
-
-e = tkinter.Entry(root, width=20)
-e.grid(row=1, column=0)
-e.insert(0, "Enter your sentence here: ")
-
+e = customtkinter.CTkEntry(frame, width=200)
+e.pack()
+e.insert(0, "Enter your sentence here:")
 
 def button_press_2():
     print(e.get())
@@ -67,14 +69,13 @@ def button_press_2():
     print('Prediction Made')
     match int(prediction[0]):
         case 0:
-            tkinter.Label(root, text="Negative Sentiment").grid()
+            customtkinter.CTkLabel(frame, text="Neutral Sentiment").pack()
         case 1:
-            tkinter.Label(root, text="Neutral Sentiment").grid()
+            customtkinter.CTkLabel(frame, text="Neutral Sentiment").pack()
         case 2:
-            tkinter.Label(root, text="Positive Sentiment").grid()
+            customtkinter.CTkLabel(frame, text="Positive Sentiment").pack()
     print(int(prediction[0]))
 
-
-tkinter.Button(root, text="Get Sentiment", command=button_press_2).grid(row=2, column=0)
+customtkinter.CTkButton(frame, text="Get Sentiment", command=button_press_2).pack(pady=6, padx=10)
 
 root.mainloop()
